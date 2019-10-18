cf848cf3e68a0cc65d8e163a1b965a85de3e6951
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-10-18 12:19:21 +0200

    Updated exercise6

--- src/the-perceptron/exercise6.py
+++ src/the-perceptron/exercise6.py
@@ -18,11 +18,16 @@ y_hat = np.array([0, 0, 1, 1, 1, 0, 1, 0, 0, 1],
 W = np.array([-0.1, -0.3, 0.2],
              dtype=np.float32)
 b = 2
+d = 10
 eta = 0.02
 ones = np.ones(10)
 
 y = np.matmul(x, W) + b
-W_new = W - eta * np.matmul(np.transpose(x), (y - y_hat))
-b_new = b - eta * np.matmul(np.transpose(ones), (y - y_hat))
+delta = y - y_hat
 
-print("New weights = ", W_new, "\nNew bias = ", b_new)
+W_new = W - eta * np.matmul(x.T, delta) * (1 / d)
+b_new = b - eta * np.matmul(ones.T, delta) * (1 / d)
+
+MSE = (1 / (2 * d)) * np.matmul(delta.T, delta)
+
+print("New weights = ", W_new.round(2), " \nNew bias = %.2f \nMSE = %.2f" % (b_new, MSE))
